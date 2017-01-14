676c9547384eb804ea36b8f73642d798a7998df8
shohitb <shohitdoon@gmail.com>
2017-01-14 15:26:00 -0500

    test

--- test/app.js
+++ test/app.js
@@ -6,10 +6,6 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MONGODB_URI){
-        connectionString = process.env.MONGODB_URI
-    }
-
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
